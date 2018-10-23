f13d04d4ce67df2a201d71b8bb75b984967ebb04
Work-PC <254282072@qq.com>
2018-10-23 10:37:35 +0800

    Initial commit

---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="main">
+        <h1>aini</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'index',
+        components: {
+
+        }
+    }
+</script>
